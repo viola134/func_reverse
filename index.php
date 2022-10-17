17b252e9195aa7a13b650abea2c6b73634530631
viola134 <violapakhomova13@gmail.com>
2022-10-17 15:17:24 +0300

    add commit

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 $fruit = ["apple", "orange", "pear", "melon", "watermelon", "peach", "kiwi", "lemon", "lime", "pineapple"];
-function cmp($a, $b)
-{
-    for ($i = 0; $i < func_num_args(); $i++) {
+function cmp($a, $b){
+    for ($i = 0; $i < func_num_args(); $i++){
         return $a <=> $b;
     }
 }
-
 uasort($fruit, "cmp");
 print_r($fruit);
